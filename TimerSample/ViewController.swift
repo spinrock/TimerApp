751e025f7f267a10485b2f209afe4267f726cd28
Satoshi Fukai <chemistreal2@gmail.com>
2017-12-05 22:32:15 +0900

    Initial Commit

---
+++ TimerSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimerSample
+//
+//  Created by Satoshi Fukai on 2017/12/05.
+//  Copyright © 2017年 Satoshi Fukai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
